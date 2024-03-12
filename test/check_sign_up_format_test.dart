99c94d95eb9fa34dbf7e2e921163688ef32f981e
nhmin08 <109854931+nhmin08@users.noreply.github.com>
2024-03-12 15:07:43 +0900

    ver0.0.1 (#3)
    Co-authored-by: nhmin08 <nhmin@synergyai.co>

--- test/check_sign_up_format_test.dart
+++ test/check_sign_up_format_test.dart
@@ -4,16 +4,13 @@ import 'package:check_sign_up_format/check_sign_up_format.dart';
 
 void main() {
   test('adds one to input values', () {
-    final validator = Validator();
-    // expect(calculator.addOne(2), 3);
-    // expect(calculator.addOne(-7), -6);
-    // expect(calculator.addOne(0), 1);
+    //final validator = Validator();
 
-    expect(validator.nameValidator("김철수", "이름 에러 메세지"), null);
-    expect(validator.idValidator("test", "아이디 에러 메세지"), null);
-    expect(validator.emailValidator("test@test.com", "이메일 에러 메세지"), null);
-    expect(validator.passwordValidator("qwer1234!", "비밀번호 에러 메세지"), null);
-    expect(validator.verifyPasswordValidator("qwer1234!", "qwer1234!", "비밀번호 확인 에러 메세지"), null);
-    expect(validator.phoneNumValidator('01012341234', "전화번호 에러 메세지"), null);
+    expect(Validator.nameValidator("김철수"), null);
+    expect(Validator.idValidator("test"), null);
+    expect(Validator.emailValidator("test@test.com"), null);
+    expect(Validator.passwordValidator("qwer1234!"), null);
+    expect(Validator.verifyPasswordValidator("qwer1234!", "qwer1234!"), null);
+    expect(Validator.phoneNumValidator('01012341234'), null);
   });
 }

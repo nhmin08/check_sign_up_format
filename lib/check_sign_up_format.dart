7b18a1ec300555fe19e21e88788718be4a6a041b
nhmin08 <nhmin@synergyai.co>
2024-03-12 15:07:20 +0900

    ver0.0.1

--- lib/check_sign_up_format.dart
+++ lib/check_sign_up_format.dart
@@ -1,54 +1,48 @@
 library check_sign_up_format;
 
-// /// A Calculator.
-// class Calculator {
-//   /// Returns [value] plus 1.
-//   int addOne(int value) => value + 1;
-// }
-
 class Validator{
-  nameValidator(String value, String? errorMessage) {
-    if (value.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'))) {
-      return errorMessage ?? "Invalid name format. Please re-enter your name.";
+  static nameValidator(String value) {
+    if (value.contains(RegExp(r'[0-9!@#$%^&*(),.?":{}|<>]'))) {
+      return "Invalid name format. Please re-enter your name.";
     }
     return null;
   }
 
-  idValidator(String value, String? errorMessage){
+  static idValidator(String value){
     if(!value.contains(RegExp(r'^[a-z0-9]*$'))){
-      return errorMessage ?? "This is not a valid ID format. Please re-enter it.";
+      return "This is not a valid ID format. Please re-enter it.";
     }
     return null;
   }
 
-  emailValidator(String value, String? errorMessage) {
+  static emailValidator(String value) {
     if (!value.contains(RegExp(
         r"^[a-zA-Z0-9.a-zA-Z0-9!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+"))) {
-      return errorMessage ?? "This email is not in a valid format. Please re-enter it.";
+      return "This email is not in a valid format. Please re-enter it.";
     }
     return null;
   }
 
-  passwordValidator(String value, String? errorMessage) {
+  static passwordValidator(String value) {
     if (value.length < 8 ||
         !value.contains(RegExp(r'(\d+)')) ||
         !value.contains(RegExp(r'[a-zA-Z]')) ||
         !value.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'))) {
-      return errorMessage ?? "Please enter a combination of alphabets, numbers, and special characters.";
+      return "Please enter a combination of alphabets, numbers, and special characters.";
     }
     return null;
   }
 
-  verifyPasswordValidator(String value, String password, String errorMessage) {
+  static verifyPasswordValidator(String value, String password) {
     if (value != password) {
-      return errorMessage ?? "The password doesn't match.";
+      return "The password doesn't match.";
     }
     return null;
   }
 
-  phoneNumValidator(String value, String errorMessage) {
+  static phoneNumValidator(String value) {
     if (!value.contains(RegExp(r'(^\d*$)'))) {
-      return errorMessage ?? "Invalid phone number format, please check and re-enter.";
+      return "Invalid phone number format, please check and re-enter.";
     }
     return null;
   }

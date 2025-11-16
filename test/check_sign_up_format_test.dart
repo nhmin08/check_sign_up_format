82b17e0c9485984c1f0a12a65742279c685171a1
nhmin08 <nhmin07@gmail.com>
2025-11-16 23:05:15 +0900

    🤡 테스트 코드 수정

--- test/check_sign_up_format_test.dart
+++ test/check_sign_up_format_test.dart
@@ -1,3 +1,4 @@
+import 'package:check_sign_up_format/regular_expression.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:check_sign_up_format/check_sign_up_format.dart';
@@ -6,11 +7,31 @@ void main() {
   test('adds one to input values', () {
     //final validator = Validator();
 
-    expect(Validator.nameValidator("김철수"), null);
-    expect(Validator.idValidator("test"), null);
-    expect(Validator.emailValidator("test@test.com"), null);
-    expect(Validator.passwordValidator("qwer1234!"), null);
-    expect(Validator.verifyPasswordValidator("qwer1234!", "qwer1234!"), null);
-    expect(Validator.phoneNumValidator('01012341234'), null);
+    expect(
+      Validator.nameValidator(
+        value: "김철수",
+        customCheck: (value) {
+          if (RegularExpression(
+            condition: Condition.specialCharacter,
+          ).validate(value)) {
+            return false;
+          } else {
+            return true;
+          }
+        },
+      ),
+      null,
+    );
+    expect(Validator.idValidator(value: "test"), null);
+    expect(Validator.emailValidator(value: "test@test.com"), null);
+    expect(Validator.passwordValidator(value: "qwer1234!"), null);
+    expect(
+      Validator.verifyPasswordValidator(
+        value: "qwer1234!",
+        password: "qwer1234!",
+      ),
+      null,
+    );
+    expect(Validator.phoneNumValidator(value: '01012341234'), null);
   });
 }

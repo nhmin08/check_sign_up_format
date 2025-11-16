51853c90ac69fc45e16c93282f315eccae27cca2
nhmin08 <nhmin07@gmail.com>
2025-11-16 23:03:53 +0900

    ✅ 에러 메세지, 정규식 커스텀 기능 추가

--- lib/check_sign_up_format.dart
+++ lib/check_sign_up_format.dart
@@ -1,49 +1,128 @@
 library check_sign_up_format;
 
-class Validator{
-  static nameValidator(String value) {
-    if (value.contains(RegExp(r'[0-9!@#$%^&*(),.?":{}|<>]'))) {
-      return "Invalid name format. Please re-enter your name.";
+import 'package:check_sign_up_format/constant/check_sign_up_constant.dart';
+
+class Validator {
+  /// 이름 형식 체크
+  static String? nameValidator({
+    required String value,
+    bool Function(String value)? customCheck,
+    String? errorMessage,
+  }) {
+    if (customCheck != null) {
+      bool isCorrect = customCheck(value);
+
+      if (!isCorrect) {
+        return errorMessage ?? CheckSignUpConstant.nameErrorMessage;
+      }
+    } else {
+      if (value.contains(RegExp(r'[0-9!@#$%^&*(),.?":{}|<>]'))) {
+        return errorMessage ?? CheckSignUpConstant.nameErrorMessage;
+      }
+    }
+
+    return null;
+  }
+
+  /// ID 형식 체크
+  static idValidator({
+    required String value,
+    bool Function(String value)? customCheck,
+    String? errorMessage,
+  }) {
+    if (customCheck != null) {
+      bool isCorrect = customCheck(value);
+
+      if (!isCorrect) {
+        return errorMessage ?? CheckSignUpConstant.idErrorMessage;
+      }
+    } else {
+      if (!value.contains(RegExp(r'^[a-z0-9]*$'))) {
+        return errorMessage ?? CheckSignUpConstant.idErrorMessage;
+      }
     }
     return null;
   }
 
-  static idValidator(String value){
-    if(!value.contains(RegExp(r'^[a-z0-9]*$'))){
-      return "This is not a valid ID format. Please re-enter it.";
+  /// email 형식 체크
+  static emailValidator({
+    required String value,
+    bool Function(String value)? customCheck,
+    String? errorMessage,
+  }) {
+    if (customCheck != null) {
+      bool isCorrect = customCheck(value);
+
+      if (!isCorrect) {
+        return errorMessage ?? CheckSignUpConstant.emailErrorMessage;
+      }
+    } else {
+      if (!value.contains(
+        RegExp(
+          r"^[a-zA-Z0-9.a-zA-Z0-9!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
+        ),
+      )) {
+        return errorMessage ?? CheckSignUpConstant.emailErrorMessage;
+      }
     }
+
     return null;
   }
 
-  static emailValidator(String value) {
-    if (!value.contains(RegExp(
-        r"^[a-zA-Z0-9.a-zA-Z0-9!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+"))) {
-      return "This email is not in a valid format. Please re-enter it.";
+  /// 비밀번호 형식 체크
+  static passwordValidator({
+    required String value,
+    bool Function(String value)? customCheck,
+    String? errorMessage,
+  }) {
+    if (customCheck != null) {
+      bool isCorrect = customCheck(value);
+
+      if (!isCorrect) {
+        return errorMessage ?? CheckSignUpConstant.passwordErrorMessage;
+      }
+    } else {
+      if (value.length < 8 ||
+          !value.contains(RegExp(r'(\d+)')) ||
+          !value.contains(RegExp(r'[a-zA-Z]')) ||
+          !value.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'))) {
+        return errorMessage ?? CheckSignUpConstant.passwordErrorMessage;
+      }
     }
+
     return null;
   }
 
-  static passwordValidator(String value) {
-    if (value.length < 8 ||
-        !value.contains(RegExp(r'(\d+)')) ||
-        !value.contains(RegExp(r'[a-zA-Z]')) ||
-        !value.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'))) {
-      return "Please enter a combination of alphabets, numbers, and special characters.";
-    }
-    return null;
-  }
-
-  static verifyPasswordValidator(String value, String password) {
+  /// 비밀번호 확인 체크
+  static verifyPasswordValidator({
+    required String value,
+    required String password,
+    String? errorMessage,
+  }) {
     if (value != password) {
-      return "The password doesn't match.";
+      return errorMessage ?? CheckSignUpConstant.verifyPasswordErrorMessage;
     }
     return null;
   }
 
-  static phoneNumValidator(String value) {
-    if (!value.contains(RegExp(r'(^\d*$)'))) {
-      return "Invalid phone number format, please check and re-enter.";
+  /// 전화번호 체크
+  static phoneNumValidator({
+    required String value,
+    bool Function(String value)? customCheck,
+    String? errorMessage,
+  }) {
+    if (customCheck != null) {
+      bool isCorrect = customCheck(value);
+
+      if (!isCorrect) {
+        return errorMessage ?? CheckSignUpConstant.emailErrorMessage;
+      }
+    } else {
+      if (!value.contains(RegExp(r'(^\d*$)'))) {
+        return errorMessage ?? CheckSignUpConstant.phoneErrorMessage;
+      }
     }
+
     return null;
   }
 }
---
+++ lib/constant/check_sign_up_constant.dart
@@ -0,0 +1,16 @@
+abstract class CheckSignUpConstant {
+  CheckSignUpConstant._();
+
+  static const nameErrorMessage =
+      'Invalid name format. Please re-enter your name.';
+  static const idErrorMessage =
+      "This is not a valid ID format. Please re-enter it.";
+  static const emailErrorMessage =
+      "This email is not in a valid format. Please re-enter it.";
+  static const passwordErrorMessage =
+      "Please enter a combination of alphabets, numbers, and special characters.";
+  static const verifyPasswordErrorMessage = "The password doesn't match.";
+
+  static const phoneErrorMessage =
+      "Invalid phone number format, please check and re-enter.";
+}
